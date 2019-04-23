d2fb2aac730a3debab66011abf5a13990c1bab34
Adrien Viviani <adrien.viv@gmail.com>
2019-04-23 17:55:00 +0800

    tuesday eveneing

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -3,13 +3,25 @@ class Api::V1::FavoritesController < Api::V1::BaseController
   def index
     # Get User's favourites
     # @favorites = Favorite.where(user_id: params[:user_id])
-    @food_array =[]
-    @favorites = Favorite.where(user_id: params[:user_id]).joins(:food)
-    @favorites.each do |favorite|
-      @fooood = Food.find_by_id(favorite.food_id)
-      @food_array << @fooood
-    end
+    # @food_array =[]
+    # @favorites = Favorite.where(user_id: params[:user_id]).joins(:food)
 
+
+    # @favorites.each do |favorite|
+    # food = Food.find(favorite.food_id)
+    # # puts favorite
+    # # food.favorite_ids = favorite.id
+    #   # @fooood.merge!(favorite_id: favorite.id)
+    # @food_array << food
+    # end
+
+    @favorites = Favorite.find_by_sql("SELECT fa.id as favorite_id, * FROM favorites as fa, foods as f WHERE fa.food_id = f.id AND fa.user_id = " + params[:user_id])
+    # @favorites.each do |favorite|
+    #   # puts favorite
+    #   # food.favorite_ids = favorite.id
+    #   # @fooood.merge!(favorite_id: favorite.id)
+    #   @food_array << food
+    # end
 
     # favorite_info = @favorites.get_product_infos()
     # @favorite = Favorite.new(favorite_info)
@@ -20,7 +32,7 @@ class Api::V1::FavoritesController < Api::V1::BaseController
 
     #construct the result
 
-    render json: @food_array
+    render json: @favorites
   end
 
   def show
@@ -40,6 +52,9 @@ class Api::V1::FavoritesController < Api::V1::BaseController
   end
 
   def destroy
+    # @favorite = Favorite.where(params[:id])
+    Favorite.destroy(params[:id])
+    head :no_content
   end
 
   private
